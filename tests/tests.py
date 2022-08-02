183d11288cf6cd38087933b70940af12c5f6bd20
serikbol <serikbol.i@gmail.com>
2022-08-02 04:06:33 +0000

    Add test files

--- tests/tests.py
+++ tests/tests.py
@@ -5,5 +5,9 @@ def test_yml_list_tables():
     if os.path.exists("Tests_soda/list_tables.yml"):
         with open('Tests_soda/list_tables.yml') as f:
             get_params = yaml.load(f, Loader=SafeLoader)
+def test_yml_pattern_tests():
+    if os.path.exists("Tests_soda/pattern_tests.yml"):
+        with open('Tests_soda/pattern_tests.yml') as f:
+            get_params = yaml.load(f, Loader=SafeLoader)
 
 
